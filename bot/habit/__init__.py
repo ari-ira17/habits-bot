3c8ad6fe030de30ee2666b5973c92213dd116ea3
ari.ira17-mcb <ari.ira17@yandex.ru>
2025-10-30 19:01:05 +0600

    chore: connect add habit router

--- bot/habit/__init__.py
+++ bot/habit/__init__.py
@@ -5,8 +5,10 @@ from aiogram import Router
 
 from .habit_by_day import router as habit_by_day_router
 from .scheduler import router as scheduler_router
+from .add_habit import router as add_habit_router
 
 router = Router(name=__name__)
 
 router.include_router(habit_by_day_router)
 router.include_router(scheduler_router)
+router.include_router(add_habit_router)
