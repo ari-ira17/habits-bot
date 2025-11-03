2fc171d85c1c46b3322a5d940fddb50c3db001b1
smnk-mcb <semikinnikolay1@gmail.com>
2025-11-03 18:02:22 +0600

    feat: db shema

--- bot/main.py
+++ bot/main.py
@@ -3,8 +3,10 @@ from create_bot import bot, dp
 from handlers import router as common_router
 from habit import router as habit_router
 from create_bot import scheduler
+from db import check_db_connection_and_schema
 
 async def main():
+    await check_db_connection_and_schema()
     dp.include_router(common_router)
     dp.include_router(habit_router)
     await bot.delete_webhook(drop_pending_updates=True)
