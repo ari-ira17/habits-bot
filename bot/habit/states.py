d89ec01e86edb1d3edca87e57fdd49dcb0671a17
ari.ira17-mcb <ari.ira17@yandex.ru>
2025-11-26 17:21:40 +0600

    feat: add state for location

--- bot/habit/states.py
+++ bot/habit/states.py
@@ -1,12 +1,9 @@
-# поля привычек, которые потом идут в БД
-
 from aiogram.fsm.state import StatesGroup, State
 
 class Habit_By_Week(StatesGroup):
     habit_id = ""
     owner_id = State()
     title = State()
-    status = 0
     period = State()
     weekdays = State()
     time_to_check = State()
@@ -16,10 +13,10 @@ class Habit_By_Days(StatesGroup):
     habit_id = ""
     owner_id = State()
     title = State()
-    status = 0
     num_days = State()
     time_to_check = State()
     
 
 class AskLocation(StatesGroup):
-    waiting_for_location = State()    
+    waiting_for_location = State()   
+     
