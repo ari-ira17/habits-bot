f0dce4f7f1d06665fa8fe72d894d79f2708ed3f8
ari.ira17-mcb <ari.ira17@yandex.ru>
2025-11-26 17:24:27 +0600

    feat: add reference to completion

--- bot/habit/__init__.py
+++ bot/habit/__init__.py
@@ -8,6 +8,7 @@ from .habit_by_day import router as habit_by_day_router
 from .habit_by_week import router as habit_by_week_router
 from .scheduler import router as scheduler_router
 from .timezone import router as timezone_router
+from .completion import router as completion_router
 
 router = Router(name=__name__)
 
@@ -16,3 +17,4 @@ router.include_router(habit_by_day_router)
 router.include_router(habit_by_week_router)
 router.include_router(scheduler_router)
 router.include_router(timezone_router)
+router.include_router(completion_router)
