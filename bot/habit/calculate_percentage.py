8d4b5cb5e8c0655cb55e033900f17452284ca389
ari.ira17-mcb <ari.ira17@yandex.ru>
2025-12-02 18:49:19 +0600

    feal: add calculating percentage

--- bot/habit/calculate_percentage.py
+++ bot/habit/calculate_percentage.py
@@ -2,6 +2,7 @@ from sqlalchemy import select, func
 from aiogram import Router
 import sys
 import os
+import datetime
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'bot'))
 from models import HabitCompletion
@@ -15,5 +16,20 @@ async def calculate_completion_percentage(habit_id: int) -> int:
             select(func.count(HabitCompletion.id)).where(HabitCompletion.habit_id == habit_id)
         )
         completion_count = completion_count_result.scalar() or 0
-        percentage = min(100, (completion_count / 20) * 100)
+        percentage = min(100, (completion_count / 10) * 100)
         return int(percentage)
+    
+
+async def calculate_weekly_completion_percentage(habit_id: int, target_date: datetime) -> int:
+
+    async for session in get_db():
+        completion_count_result = await session.execute(
+            select(func.count(HabitCompletion.id)).where(
+                HabitCompletion.habit_id == habit_id,
+                HabitCompletion.completed_at <= target_date
+            )
+        )
+        completion_count = completion_count_result.scalar() or 0
+        percentage = min(100, (completion_count / 10) * 100)
+        return int(percentage)
+    
