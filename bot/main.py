f3c79dc6b29d4b357413c9f9822432dddd0067bc
ari.ira17-mcb <ari.ira17@yandex.ru>
2025-12-03 08:05:07 +0600

    style: remove breaklines

--- bot/main.py
+++ bot/main.py
@@ -4,25 +4,8 @@ from create_bot import bot, dp
 from handlers import router as common_router
 from habit import router as habit_router
 from db import check_db_connection_and_schema, engine
-from models import Base, Habit
-
-async def show_habits():
-    from db import get_db
-    from sqlalchemy import select
-
-    async for session in get_db():
-        result = await session.execute(select(Habit))
-        habits = result.scalars().all()
-
-        if habits:
-            print("--- Habits ---")
-            for habit in habits:
-                print(f"ID: {habit.id}, User ID: {habit.user_id}, Name: {habit.name}, "
-                      f"Active: {habit.is_active}, Config: {habit.reminder_config}, "
-                      f"Last Reminded: {habit.last_reminded_at}, Next Reminder: {habit.next_reminder_datetime_utc}")
-        else:
-            print("Таблица habits пуста.")
-        break
+from models import Base
+from habit.scheduler import start_scheduler
 
 
 async def main():
@@ -31,12 +14,9 @@ async def main():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    await show_habits()
-
     dp.include_router(common_router)
     dp.include_router(habit_router)
 
-    from habit.scheduler import start_scheduler
     start_scheduler(bot)
 
     await bot.delete_webhook(drop_pending_updates=True)
